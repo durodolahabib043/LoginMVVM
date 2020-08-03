d92314d533aa2bdb10e368af9da1f2b8a0181817
habib <durodolahabib043@gmail.com>
2020-08-03 11:22:26 -0400

    first commit

---
+++ LoginModelView/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  LoginModelView
+//
+//  Created by Habib Durodola on 2020-08-03.
+//  Copyright © 2020 DOH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var userNameTxt: UITextField!
+    @IBOutlet weak var passWordTxt: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
